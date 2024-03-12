c8a736cf5dbb3b0b915407ca02fe0768f3e22cb1
haruki <meteo.haru@gmail.com>
2024-03-12 11:01:49 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Koibumi Design System CSS
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+
+
+function compileAndConcatCss() {
+
+    return gulp.src(['src/css/global.css', 'src/sass/utils.scss', 'src/sass/components/*.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('dist'))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+}
+
+
+exports.default = compileAndConcatCss;
